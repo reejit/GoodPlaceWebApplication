30c45fdc23f3affe95be34686cbee027a5f48226
BrunoCoric <bruno.coric@fer.hr>
2020-12-01 15:52:04 +0100

    popravljen

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask,jsonify,request, render_template
 import io
 from PIL import Image
-#from torch_utils import get_prediction, transform_image
+from torch_utils import get_prediction, transform_image
 
 app = Flask(__name__)
 
@@ -15,22 +15,22 @@ def allowed_file(filename):
 def render_page():
     return render_template('index.html')
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if request.method == 'POST':
-#         file = request.files.get('image')
-#         if file is None or file.filename == "":
-#             return jsonify({'error': 'no file'})
-#         if not allowed_file(file.filename):
-#             return jsonify({'error': 'format not supported'})
-#
-#         try:
-#             img_bytes = file.read()
-#             image = io.BytesIO(img_bytes)
-#             image = Image.open(image)
-#             tensor = transform_image(image)
-#             prediction = get_prediction(tensor)
-#             data = {'prediction': prediction}
-#             return jsonify(data)
-#         except:
-#             return jsonify({'error': 'error during prediction'})
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files.get('image')
+        if file is None or file.filename == "":
+            return jsonify({'error': 'no file'})
+        if not allowed_file(file.filename):
+            return jsonify({'error': 'format not supported'})
+
+        try:
+            img_bytes = file.read()
+            image = io.BytesIO(img_bytes)
+            image = Image.open(image)
+            tensor = transform_image(image)
+            prediction = get_prediction(tensor)
+            data = {'prediction': prediction}
+            return jsonify(data)
+        except:
+            return jsonify({'error': 'error during prediction'})
